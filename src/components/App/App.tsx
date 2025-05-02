146d615941a8ce2855da8e2a71b382b5956c828e
Alex Arena <arena.secure@gmail.com>
2025-05-02 10:20:29 -0400

    adds tailwind as an option for styling

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,12 @@
-import "./app.css";
+// The project includes Tailwind, so feel free to use it to style your components.
+// If you don't know/like Tailwind, all styling can be accomplished with just inline styles.
 
 function App() {
-  return <div style={{ padding: "2rem" }}>Your code goes here!</div>;
+  return (
+    <div style={{ padding: "2rem" }} className="font-medium">
+      Your code goes here!
+    </div>
+  );
 }
 
 export default App;
